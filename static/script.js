62ea48b4dff82760bd970c64a5653bee57e2bf26
ismail-farooq <ismail_farooq@students.ocean.edu>
2024-04-19 10:10:52 +0300

    commit

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.container');
-const search = document.querySelector('.search-box button');
+const search = document.querySelector('.search-box .search');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
